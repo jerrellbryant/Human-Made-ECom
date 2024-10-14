c6d800078f4b310c8f7a6b28a798ed8345548956
jerrellbryant <jerrellkbryant@icloud.com>
2024-10-14 15:50:34 -0400

    updated header with gsap animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,43 @@
+"use client"
+
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(useGSAP);
 
 type Props = {}
 
 export default function Header({}: Props) {
-  return (
-        <header className="flex justify-center py-4 -mb-28 ">
-              <div className="w-full Z-10 max-w-[200px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] animate-logo">
-           <img 
-              src="./HumanMade-logo.png" 
-              alt="Human-Made Logo" 
-              className="w-full h-auto"
-            />
-            </div>
+  useGSAP(() => {
+    const animateIntro = gsap.timeline();
 
-         </header>
+    animateIntro.fromTo(".logo-animate", 
+      { 
+        opacity: 0,
+        y: -200,
+        scale: 0.8,
+      },
+      { 
+        opacity: 1,
+        duration: 1.5,
+        y: 0,
+        scale: 1,
+        ease: "elastic.out(1, 0.3)",
+        transformOrigin: "center bottom"
+      }
+    );
+  });
+
+  return (
+    <header className="header flex justify-center py-4 -mb-28">
+      <div className="logo-animate opacity-0 w-full z-10 max-w-[200px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px]">
+        <img 
+          src="./HumanMade-logo.png" 
+          alt="Human-Made Logo" 
+          className="w-full h-auto"
+        />
+      </div>
+    </header>
   )
 }
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -112,7 +112,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
            
 
             <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-[#e6460f]
-            md:text-[9rem] lg:text-[13rem]">
+            md:text-[9rem] lg:text-[10rem]">
               <TextSplitter text={asText(slice.primary.heading)} wordDisplayStyle="block" className="hero-header-word"/>
             </h1>
 
